484e2aec7cafb7b28c3271c5d514c97e419a88e3
Apurv Jain <jainapurv35@gmail.com>
2023-01-30 17:54:20 +0530

    Frontend with styles

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import { Box } from '@mui/system';
 import { useSnackbar,SnackbarProvider } from 'notistack'
+import { Button, Divider, IconButton, LinearProgress, Stack, Typography } from '@mui/material';
+import DownloadForOfflineRounded from '@mui/icons-material/DownloadForOfflineRounded';
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import { useState } from 'react';
 
 export default function App() {
   
@@ -24,13 +28,23 @@ export default function App() {
   const [object, setObject] = React.useState('');
 
   const handleChangeObject = (event) => {
-    if(app=="" && action=="") alert('Please select application and object');
-    else if(action=="") alert('Please select an object');
+    if(app=="" && action=="") alert('Please select application and action');
+    else if(action=="") alert('Please select an action');
     else setObject(event.target.value);
   };
+  const onDownload = () => {
+    const link = document.createElement("a");
+    link.download = `download.txt`;
+    link.href = "./download.txt";
+    link.click();
+  };
+
   return (
-    <SnackbarProvider>
-<Box sx={{display:'flex',justifyContent:'space-around'}} mt={5}>
+    <Box sx={{backgroundColor:'#EEF1EF !important'}}  pt = {5} pl={10} pr={10} pb = {5} >
+    <Typography  variant="h5">
+        Upload data
+      </Typography>
+    <Box sx={{display:'flex',justifyContent:'space-between'}}  mb={2} mt={2}>
       <FormControl sx={{ minWidth: 140 }}>
         <InputLabel id="demo-simple-select-readonly-label">Application</InputLabel>
         <Select
@@ -41,8 +55,8 @@ export default function App() {
           autoWidth
           label="Application"
           placeholder='Application'
+          size='medium'
         >
-          {/* <MenuItem value={true}>Select Application</MenuItem> */}
           <MenuItem value="p247">P247</MenuItem>
           <MenuItem value="bf">BF</MenuItem>
           <MenuItem value="rummy">Rummy</MenuItem>
@@ -51,15 +65,14 @@ export default function App() {
       <FormControl sx={{  minWidth: 140 }}>
         <InputLabel id="demo-simple-select-readonly-label">Action</InputLabel>
         <Select
-          labelId="demo-simple-select-readonly-label"
-          id="demo-simple-select-readonly"
+          labelId="demo-simple-select-readonly-label-small"
+          id="demo-simple-select-readonly-small"
           value={action}
           onChange={handleChangeAction}
           autoWidth
           label="Action"
           placeholder='Action'
         >
-          {/* <MenuItem value={true}>Select Application</MenuItem> */}
           <MenuItem value="add">Add new data</MenuItem>
           <MenuItem value="update">Update data</MenuItem>
           <MenuItem value="delete">Delete data</MenuItem>
@@ -76,7 +89,6 @@ export default function App() {
           label="Object"
           placeholder='Object'
         >
-          {/* <MenuItem value={true}>Select Application</MenuItem> */}
           <MenuItem value="category">Category</MenuItem>
           <MenuItem value="properties">Properties</MenuItem>
           <MenuItem value="leads">Leads</MenuItem>
@@ -84,7 +96,18 @@ export default function App() {
         </Select>
       </FormControl>
     </Box>
-    </SnackbarProvider>
-    
+    <Divider />
+    <Box sx={{display:'flex',flexDirection:'column',alignItems:'center'}} mt={4} mb={2}>
+    <Button variant="outlined" endIcon={<DownloadForOfflineRounded/>} onClick={onDownload}>
+      Download sample file
+    </Button>
+    <Button variant="contained" component="label" sx={{marginTop:'1.5rem'}}>
+        Choose files
+        <input hidden accept={".csv"} multiple type="file" />
+      </Button>
+    </Box>
+    <LinearProgress/>
+    </Box>
+
   );
-}
+  }
